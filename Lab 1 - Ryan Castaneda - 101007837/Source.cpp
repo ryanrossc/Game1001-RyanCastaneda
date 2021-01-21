f40d2423d0b55e0191a121325fa246753621132e
ryan <ryan.castaneda@georgebrown.ca>
2021-01-20 22:07:30 -0500

    Lab examples

---
+++ Lab 1 - Ryan Castaneda - 101007837/Source.cpp
@@ -0,0 +1,24 @@
+// A simple C++ Program
+/*
+Hi my name is Ryan
+101007837
+*/
+#include <iostream>
+using namespace std; // Use the standard namespace
+
+// Main entry point to the application
+int main()
+{
+	int number; // Data type + unique identifiers
+
+	int checking;
+	unsigned miles;
+	long days;
+
+	checking = -20;
+	miles = 4276;
+	days = 192000;
+
+	cout << "i definitely did not travel " << miles << " miles this year";
+		return 0;
+}
